49eae84deaefd34521ac8e2b2ed7c836dff5baac
Sandesh <sandeshsudake818@gmail.com>
2025-08-12 09:22:04 +0530

    added cross origin to health check controller for testing only

--- src/main/java/com/sandeshsudake/MesaV2/controller/healthCheck.java
+++ src/main/java/com/sandeshsudake/MesaV2/controller/healthCheck.java
@@ -1,8 +1,10 @@
 package com.sandeshsudake.MesaV2.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class healthCheck {
 
     @GetMapping("/healthCheck")
